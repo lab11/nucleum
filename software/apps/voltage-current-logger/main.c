a75edd5fa66d82defdd3489bf37a4607acd5555c
Josh Adkins <adkinsjd@umich.edu>
2016-06-23 17:28:28 -0400

    updated voltage current logger

--- software/apps/voltage-current-logger/main.c
+++ software/apps/voltage-current-logger/main.c
@@ -7,29 +7,39 @@
 // nrf5x-base libraries
 #include "simple_ble.h"
 #include "simple_adv.h"
+#include "multi_adv.h"
+#include "eddystone.h"
 #include "simple_timer.h"
 #include "nrf_gpio.h"
 #include "led.h"
 #include "nucleum_logger.h"
 #include "cvsense.h"
 #include "rv3049.h"
-
 #include "simple_logger.h"
 
+#define ADV_SWITCH_MS 1000
+#define PHYSWEB_URL "goo.gl/qtn9V9"
+#define CV_SENSE_SERVICE 0x17
+#define UMICH_COMPANY_IDENTIFIER 0x02E0
 
-ble_advdata_manuf_data_t mandata;
+typedef struct {
+	float volt;
+	float curr;
+	float pow;
+} __attribute__((packed)) cv_data_t;
+
+cv_data_t cv_data;
+uint8_t mdata[1 + sizeof(cv_data_t)];
 
 // Intervals for advertising and connections
 static simple_ble_config_t ble_config = {
     .platform_id       = 0x00,              // used as 4th octect in device BLE address
     .device_id         = DEVICE_ID_DEFAULT,
-    .adv_name          = DEVICE_NAME,       // used in advertisements if there is room
+    .adv_name          = "Solar",       // used in advertisements if there is room
     .adv_interval      = MSEC_TO_UNITS(500, UNIT_0_625_MS),
     .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
     .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS)
 };
-
-static ble_uuid_t TEST_SERVICE_UUID = {0xBEAF, BLE_UUID_TYPE_BLE};
 
 void log_data(void* p_context) {
 
@@ -37,10 +47,17 @@ void log_data(void* p_context) {
 	rv3049_read_time(&time);
 
 	float voltage = cvsense_get_voltage();
+	cv_data.volt = voltage;
 
 	cvsense_short_circuit();
 	for(volatile int i = 0; i < 1000000; i++);
+
 	float current = cvsense_get_current();
+	cvsense_open_circuit();
+
+	cv_data.curr = current;
+
+	cv_data.pow = current*voltage*0.7;
 
 	cvsense_open_circuit();
 
@@ -48,6 +65,23 @@ void log_data(void* p_context) {
 													voltage,current,voltage*current*0.7);
 }
 
+static void adv_config_eddystone () {
+	eddystone_adv(PHYSWEB_URL, NULL);
+}
+
+static void adv_config_data() {
+	ble_advdata_manuf_data_t mandata;
+
+	mdata[0] = CV_SENSE_SERVICE;
+
+	memcpy(mdata+1, (uint8_t*) &cv_data, sizeof(cv_data_t));
+
+	mandata.company_identifier = UMICH_COMPANY_IDENTIFIER;
+	mandata.data.p_data = mdata;
+	mandata.data.size = 1 + sizeof(cv_data_t);
+		
+	simple_adv_manuf_data(&mandata);
+}
 
 int main(void) {
 	
@@ -58,7 +92,10 @@ int main(void) {
 
     // Setup BLE
     simple_ble_init(&ble_config);
-    simple_adv_service_manuf_data(&TEST_SERVICE_UUID, &mandata);
+
+	multi_adv_init(ADV_SWITCH_MS);
+	multi_adv_register_config(adv_config_eddystone);
+	multi_adv_register_config(adv_config_data);
 
 	//setup timer
 	simple_timer_init();
@@ -69,6 +106,8 @@ int main(void) {
 
 	//start the timer
 	simple_timer_start (10000, log_data);
+
+	multi_adv_start();
 
     while (1) {
 		simple_logger_update();
